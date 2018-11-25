4f2d0b383eaa6c7be72e57df980cb9abdc8467a2
Elvar <eojons@gmail.com>
2018-11-25 16:56:59 +0000

    Basics
    Board and cells

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+    public GameObject mCellPrefab;
+
+    private int mXSize = 8;
+    private int mYSize = 8;
+
+    private int GridPixelCount = 100;
+    private int GridPixelOffset = 50;
+
+    [HideInInspector]
+    public Cell[,] mAllCells = new Cell[8, 8]; // Hmmm...
+
+    public void Create()
+    {
+        for (int y = 0; y < mYSize; y++)
+        {
+            for (int x = 0; x < mXSize; x++)
+            {
+                // Create cell
+                GameObject newCell = Instantiate(mCellPrefab, transform);
+
+                // Position
+                RectTransform rectTransfrom = newCell.GetComponent<RectTransform>();
+                rectTransfrom.anchoredPosition = new Vector2((x * GridPixelCount) 
+                    + GridPixelOffset, (y * GridPixelCount) + GridPixelOffset);
+
+                // Setup
+                mAllCells[x, y] = newCell.GetComponent<Cell>();
+                mAllCells[x, y].Setup(new Vector2Int(x, y), this);
+            }
+        }
+    }
+}
